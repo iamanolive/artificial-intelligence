66a2b370b11042b4d4c2c925904a3b8ad0398df4
iamanolive <theolivequeen.xyz@gmail.com>
2024-05-19 03:24:01 +0300

    python lecture 9

--- the-python-project/the-university-course/lecture-9/program.py
+++ the-python-project/the-university-course/lecture-9/program.py
@@ -3,5 +3,6 @@ han = open("mbox.txt")
 for line in han:
     line = line.rstrip()
     wds = line.split()
-    if wds[0] != "from": continue
+    if len(wds) > 0 and wds[0] != "from":
+        continue
     print(wds[2])
