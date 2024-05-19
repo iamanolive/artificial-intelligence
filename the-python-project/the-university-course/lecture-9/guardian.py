1c109681ad5ab677b8f7f5b96eeee3ca4f5f085a
anoushka <theolivequeen.xyz@gmail.com>
2024-05-19 04:12:25 +0300

    python lecture 9

--- the-python-project/the-university-course/lecture-9/guardian.py
+++ the-python-project/the-university-course/lecture-9/guardian.py
@@ -3,6 +3,6 @@ han = open("mbox.txt")
 for line in han:
     line = line.rstrip()
     wds = line.split()
-    if len(wds) > 0 and wds[0] != "from":
+    if len(wds) > 2 or wds[0] != "from":
         continue
     print(wds[2])
---
+++ the-python-project/the-university-course/lecture-9/loops.py
@@ -0,0 +1,8 @@
+counts = {
+    "chuck": 1,
+    "fred": 42,
+    "jan": 10
+    }
+
+for key in counts:
+    print(key, counts[key])
