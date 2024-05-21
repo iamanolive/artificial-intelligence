bc7b224f2c454338a034c8bdabdc4233c826a571
anoushka <theolivequeen.xyz@gmail.com>
2024-05-21 20:52:21 +0300

    web scraper

--- the-web-scraping-project/the-selenium-project/leetcode-scraper.py
+++ the-web-scraping-project/the-selenium-project/leetcode-scraper.py
@@ -1,10 +1,10 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 
-web = "https://leetcode.com/problemset/?page=20"
+web = "https://leetcode.com/problemset/?page=18"
 path = "C:\Program Files (x86)\chromedriver.exe"
 
-file = open("leetcode-scraper-2.txt", "w")
+file = open("leetcode-scraper.txt", "a")
 
 service = Service(executable_path = path)
 driver = webdriver.Chrome(service = service)
@@ -15,12 +15,11 @@ problems = driver.find_elements(by = "xpath", value = "//div[contains(@class, 'o
 
 for problem in problems:
     file.write("{\n")
-    file.write("\tproblemName: " + "\"" + problem.find_element(by = "xpath", value = ".//a[contains(@class, 'h-5')]").text + "\",\n")
-    file.write("\tdifficulty: " + "\"" + problem.find_element(by = "xpath", value = ".//div[contains(@style, 'flex: 84 0 auto')]").text + "\",\n")
-    file.write("\tleetCodeLink: " + "\"" + problem.find_element(by = "xpath", value = ".//a[contains(@class, 'h-5')]").get_attribute("href") + "\",\n")
-    file.write("\tsolutionLink: \"#\"\n")
+    file.write("\t\"problemName\": " + "\"" + problem.find_element(by = "xpath", value = ".//a[contains(@class, 'h-5')]").text + "\",\n")
+    file.write("\t\"difficulty\": " + "\"" + problem.find_element(by = "xpath", value = ".//div[contains(@style, 'flex: 84 0 auto')]").text + "\",\n")
+    file.write("\t\"leetCodeLink\": " + "\"" + problem.find_element(by = "xpath", value = ".//a[contains(@class, 'h-5')]").get_attribute("href") + "\",\n")
+    file.write("\t\"solutionLink\": \"#\"\n")
     file.write("},\n")
 
 file.close()
 driver.quit()
-
