75c36b6e02bac82f238d55c4f51eacc46a437143
iamanolive <theolivequeen.xyz@gmail.com>
2024-06-23 13:46:07 +0300

    lecture 03

---
+++ 01-the-python-project/01-programming-in-python/lecture-03/conditional-steps.py
@@ -0,0 +1,6 @@
+x = 5
+if x < 10:
+    print("smaller")
+if x > 100:
+    print("bigger")
+print("finish")
---
+++ 01-the-python-project/01-programming-in-python/lecture-03/two-way-decisions.py
@@ -0,0 +1,4 @@
+x = 4
+if x > 2: print("bigger")
+else: print("smaller")
+print("all done")
