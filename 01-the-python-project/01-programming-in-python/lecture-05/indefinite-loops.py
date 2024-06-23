3c79c71d8c4f6312ea3841651edfbb3a9a4b89b9
iamanolive <theolivequeen.xyz@gmail.com>
2024-06-23 15:08:24 +0300

    lecture 05

---
+++ 01-the-python-project/01-programming-in-python/lecture-05/indefinite-loops.py
@@ -0,0 +1,21 @@
+# the while or indefinite loop
+n = 5
+while n > 0:
+    print(n)
+    n = n - 1
+print("blastoff!")
+print(n)
+
+# an infinite loop
+n = 5
+while n > 0:
+    print("lather")
+    print("rinse")
+print("dry off")
+
+# zero-trip loops
+n = 0
+while n > 0:
+    print("lather")
+    print("rinse")
+print("dry off")
